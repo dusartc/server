0ba546cbc1bec0b22c076ae8d2a9881ec204021c
Clement DUSART <clement.dusart@etudiant.univ-lille1.fr>
2016-01-27 17:23:07 +0100

    socket

--- webserver/main.c
+++ webserver/main.c
@@ -1,11 +1,20 @@
+#include "socket.h"
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
-int main(int argc,char **argv){
-  if (argc > 1 && strcmp(argv[1], "-advice") == 0){
-    printf("Don't Panic!\n");
-    return 42;
-  }
-  printf("Need an advice?\n");
-  return 0;
+#include <sys/types.h>
+#include <sys/socket.h>
 
+int main(void){
+  int socket_client, socket_serveur = creer_serveur(8080);
+  socket_client = accept(socket_serveur, NULL, NULL);
+  if(socket_client == -1){
+    perror("socket client");
+    exit(EXIT_FAILURE);
+  }
+
+  const char* mess = "bonjour";
+  write(socket_client, mess, strlen(mess));
+  return 0;
 }
---
+++ webserver/socket.c
@@ -0,0 +1,31 @@
+#include "socket.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+int creer_serveur(int port){
+  int socket_serveur;
+  socket_serveur = socket(AF_INET, SOCK_STREAM, 0);
+  if(socket_serveur == -1){
+    perror("socket_serveur");
+    fprintf(stderr,"erreur a la creation du socket");
+    exit(EXIT_FAILURE);
+  }
+  struct sockaddr_in saddr;
+  saddr.sin_family = AF_INET;
+  saddr.sin_port = htons(port);
+  saddr.sin_addr.s_addr = INADDR_ANY;
+
+  if(bind(socket_serveur, (struct sockaddr *)&saddr, sizeof(saddr)) == -1){
+    perror("bind socket");
+    fprintf(stderr, "erreur au bind du socket");
+    exit(EXIT_FAILURE);
+  }
+  if(listen(socket_serveur, 10) == -1){
+    perror("listen socket_serveur");
+    exit(EXIT_FAILURE);
+  }
+  return socket_serveur;
+}
