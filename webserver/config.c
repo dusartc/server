64271ad79845aa237da455fdcd94c28483f7d77a
Benjamin Deregnaucourt <Benjamin.Deregnaucourt@univ-lille1.fr>
2016-02-03 17:20:23 +0100

    début correction zombies

--- webserver/config.c
+++ webserver/config.c
@@ -1,8 +1,20 @@
 #include <signal.h>
 #include <stdio.h>
 
+void traitement_signal(int sig){
+  printf("Signal reçu\n");
+}
+
 void initialiser_signaux(void){
   if(signal(SIGPIPE, SIG_IGN) == SIG_ERR){
     perror("signal");
   }
+  struct sigaction sa;
+  sa.sa_handler = traitement_signal;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = SA_RESTART ;
+  if (sigaction(SIGCHLD, &sa, NULL) == -1)  {
+    perror ( " sigaction ( SIGCHLD ) " );
+  }
 }
+
--- webserver/main.c
+++ webserver/main.c
@@ -11,6 +11,8 @@
 
 int main(void){
   int socket_client, socket_serveur = creer_serveur(8080);
+
+  initialiser_signaux();
   while(1){
     socket_client = accept(socket_serveur, NULL, NULL);
     if(fork()==0){
@@ -18,8 +20,8 @@ int main(void){
         perror("socket client");
         exit(EXIT_FAILURE);
       }
-      initialiser_signaux();
-      const char* mess = "Bonjour, nous vous remercions d'avoir choisi notre serveur.\nPour vous montrer notre gratitude voici un recette de cordon bleu.\n\nTout d'abord aller dans la grande surface la plus proche pour vous\nprocurer l'élément principal, votre paquet de cordons bleu surgelés.\n\nUne fois cette étape terminée, faites préchauffer votre four.\nfaites chauffer les cordons bleu, bravo vous avez reussi.\nVous pouvez donc dès maintenant déguster un met fin et délicat.\n\nJe sais que cela nous vous suffit pas gredin\nje vais donc vous expliquer comment faire du flan, tout d'abord allez chez le fermier volez-lui une vache\nvous récuperer le lait.\nMaintenant on fait bouillir le lait et on ajoute le sachet magique et vous mélangez.\n\nBravo vous savez préparer le président de la République !\n";
+      const char* mess = "Bonjour, nous vous remercions d'avoir choisi notre serveur.\nPour vous montrer notre gratitude voici un recette de cordon bleu.\n\nTout d'abord aller dans la grande surface la plus proche pour vous\nprocurer l'élément principal, votre paquet de cordons bleu surgelés.\n\nUne fois cette étape terminée, faites préchauffer votre four.\nFaites chauffer les cordons bleu, bravo vous avez reussi.\nVous pouvez donc dès maintenant déguster un met fin et délicat.\n\nJe sais que cela nous vous suffit pas petite gourgandine, \nje vais donc vous expliquer comment faire du flan, tout d'abord allez chez le fermier volez-lui une vache, vous récuperez le lait.\nMaintenant on fait bouillir le lait et on ajoute le sachet magique et vous mélangez.\n\nBravo vous savez préparer le président de la République !\n\n";
+
       sleep(1);
       write(socket_client, mess, strlen(mess));
       char buffer[BUFFER_SIZE];
