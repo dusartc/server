208430d16af95a766113c6e12d2625d124e14308
Clement DUSART <clement.dusart@etudiant.univ-lille1.fr>
2016-02-03 15:05:38 +0100

    renvoie le message que le client lui envoie

--- webserver/main.c
+++ webserver/main.c
@@ -8,13 +8,29 @@
 
 int main(void){
   int socket_client, socket_serveur = creer_serveur(8080);
-  socket_client = accept(socket_serveur, NULL, NULL);
-  if(socket_client == -1){
-    perror("socket client");
-    exit(EXIT_FAILURE);
-  }
+  while(1){
+    socket_client = accept(socket_serveur, NULL, NULL);
+    if(socket_client == -1){
+      perror("socket client");
+      exit(EXIT_FAILURE);
+    }
 
-  const char* mess = "bonjour";
-  write(socket_client, mess, strlen(mess));
-  return 0;
+    const char* mess = "bonjour\n";
+    write(socket_client, mess, strlen(mess));
+    sleep(1);
+
+    char buffer[1024];
+    while(1){
+      memset(buffer, '\0', 1024);
+      int n=0;
+      if((n=read(socket_client, buffer, 1024))==-1){
+        perror("read");
+        return EXIT_FAILURE;
+      }
+      write(socket_client,buffer, strlen(buffer));
+    }
+  }
+  close(socket_client);
+  close(socket_serveur);
+  return EXIT_SUCCESS;
 }
