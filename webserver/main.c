72d4259db5b2b80b895266220923b74531397ad5
Clement DUSART <clement.dusart@etudiant.univ-lille1.fr>
2016-03-02 15:44:30 +0100

    404

--- webserver/main.c
+++ webserver/main.c
@@ -12,8 +12,11 @@
 
 const char* mess = "Bonjour, nous vous remercions d'avoir choisi notre serveur.\nPour vous montrer notre gratitude voici une recette de cordon bleu.\n\nTout d'abord aller dans la grande surface la plus proche pour vous\nprocurer l'élément principal, votre paquet de cordons bleu surgelés.\n\nUne fois cette étape terminée, faites préchauffer votre four.\nFaites chauffer les cordons bleu, bravo vous avez reussi.\nVous pouvez donc dès maintenant déguster un mets fin et délicat.\n\nJe sais que cela nous vous suffit pas petite gourgandine, \nje vais donc vous expliquer comment faire du flan, tout d'abord allez chez le fermier, volez-lui une vache, vous récuperez le lait.\nMaintenant on fait bouillir le lait et on ajoute le sachet magique et vous mélangez.\n\nBravo vous savez préparer le président de la République !\n\n";
 
-const char* error404 = "HTTP/1.1 400 Bad Request\r\nConnection: close\
-                        \r\nContent-Length: 17\r\n\r\n400 Bad request";
+const char* error400 = "HTTP/1.1 400 Bad Request\r\nConnection: close\
+                        \r\nContent-Length: 17\r\n\r\n400 Bad request\r\n";
+
+const char* error404 = "HTTP/1.1 404 File Not Found\r\nContent-Length:\
+                         18\r\n\r\n404 File not found\r\n";
 
 char* ok = "HTTP/1.1 200 OK\r\nContent-Length: %d\r\n\r\n%s";
 
@@ -37,19 +40,23 @@ int main(void){
       char *buf;
       while(1){
         memset(buffer, '\0', BUFFER_SIZE);
-        int get=0, done=0;
+        int get=0, done=0, toto=0;
         while((buf=fgets(buffer, BUFFER_SIZE, server))!=NULL){
           printf("%s", buffer);
-          if(parse(buffer) == 1) get = 1;
+          if((toto = parse(buffer)) != 0) get = toto;
           if(strcmp(buffer, "\r\n") == 0) done = 1;
           if(get == 1 && done == 1){
             fprintf(server, ok, strlen(mess),mess);
             fflush(server);
+            get=0;done=0;toto=0;
+          } else if(get == 2 && done == 1){
+            fprintf(server, error404);
+            fflush(server);
+            get=0;done=0;toto=0;
           } else if(done == 1){
-            if(fwrite(error404, strlen(error404), 1, server) == 0){
-              perror("fwrite");
-              return EXIT_FAILURE;
-            }
+            fprintf(server, error400);
+            fflush(server);
+            get=0;done=0;toto=0;
           }
         }
       }
--- webserver/parser.c
+++ webserver/parser.c
@@ -14,9 +14,16 @@ int parse(char *str){
     printf("\t%s\n", token);
     if(strcmp(token,"GET")==0){
       token = strtok(NULL, delim);
-      token = strtok(NULL, delim);
-      if(strcmp(token, "HTTP/1.0\r\n") == 0 || strcmp(token, "HTTP/1.1\r\n") == 0){
-        return 1;
+      if(strcmp(token,"/")==0){
+        token = strtok(NULL, delim);
+        if(strcmp(token, "HTTP/1.0\r\n") == 0 || strcmp(token, "HTTP/1.1\r\n") == 0){
+          return 1;
+        }
+      } else {
+        token = strtok(NULL, delim);
+        if(strcmp(token, "HTTP/1.0\r\n") == 0 || strcmp(token, "HTTP/1.1\r\n") == 0){
+          return 2;
+        }
       }
     }
     token = strtok(NULL, delim);
